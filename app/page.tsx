f91cc128f6a55e4befb2ec547ae4213fdf9d500c
Abhiram A R <abhiramajithr@gmail.com>
2026-08-31 23:40:03 +0530

    a fresh start

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="w-dvw h-screen flex justify-center items-center border border-black gap-4">
-      <Button className="w-32 px-4 py-3 h-8">
-        <Link href="/login">Login</Link>
-      </Button>
-      <Button className="w-32 px-4 py-3 h-8">
-        <Link href="/app">App</Link>
-      </Button>
-    </div>
+    <></>
   );
 }
